4530c1d712b61e506ba61f441c5ad0eac606bcde
omidnb <nobahariomid@gmail.com>
2020-06-23 16:10:23 -0400

    Switch and While (Do/While)

--- Conditions,Loops/Program.cs
+++ Conditions,Loops/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace If_Else
 {
@@ -6,6 +7,7 @@ namespace If_Else
     {
         static void Main(string[] args)
         {
+            //if...else
             int age = 25;
             if (age > 21)
             {
@@ -19,6 +21,39 @@ namespace If_Else
             int num = 25;
             string result = (num > 21) ? "Adult" : "Kiddo";
             Console.WriteLine(result);
+
+            //switch
+            int meat = 5;
+            switch (meat)
+            {
+                case 1:
+                    Console.WriteLine("beef");
+                    break;
+                case 2:
+                    Console.WriteLine("fish");
+                    break;
+                case 3:
+                    Console.WriteLine("chicken");
+                    break;
+                default:
+                    Console.WriteLine("looking for some special meats");
+                    break;
+            }
+
+            //while, do/while
+            int i = 1 , j = 1;
+            while(i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j < 5);
         }
     }
 }
