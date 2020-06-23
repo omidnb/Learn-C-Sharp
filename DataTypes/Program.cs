e744dc005477f456bc2e31e9c61896f6ef483139
omidnb <nobahariomid@gmail.com>
2020-06-23 00:03:16 -0400

    different data types

---
+++ DataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int myNum = 15;   //unchangeable
+            Console.WriteLine(myNum);
+
+            int a = 25;      //use "long" for very big numbers
+            double b = 2.5D; //can use "float" too (D isn't required)
+            float c = 2.5F;  //can use "float" too (F is required)
+            string myName = "Omid";
+
+            Console.WriteLine("My name is " + myName + " and I'm " + a + " years old.");
+        }
+    }
+}
