17c116ccf06c67d137da5d7e0f93336b4df99d72
omidnb <nobahariomid@gmail.com>
2020-06-23 15:20:48 -0400

    if and else

---
+++ If,Else/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 25;
+            if(age > 21)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Kiddo");
+            }
+
+            int num = 25;
+            string result = (num > 21) ? "Adult" : "Kiddo";
+            Console.WriteLine(result);
+        }
+    }
+}
