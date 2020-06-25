a2e4b338493f103670e18e4216d35228dec12c83
omidnb <nobahariomid@gmail.com>
2020-06-24 23:49:03 -0400

    getting started

---
+++ OOP/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP
+{
+    class Country
+    {
+        public string buildIn = "Germany";
+    }
+    class Car
+    {
+        string color = "gray";
+        static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            Console.WriteLine(myObj1.color);
+
+            Country myObj2 = new Country();
+            Console.WriteLine(myObj2.buildIn);
+        }
+    }
+}
