479585084e28463b4ff4e6e3e7b486daed2b19d8
omidnb <nobahariomid@gmail.com>
2020-06-26 12:08:04 -0400

    created a constructor with 3 parameters

---
+++ Constructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Car
+    {
+        public string brand;
+        public string model;
+        public int year;
+
+        public Car(string carBrand, string carModel, int carYear)
+        {
+            brand = carBrand;
+            model = carModel;
+            year = carYear;
+        }
+
+        static void Main(string[] args)
+        {
+            Car myCar = new Car("BMW", "E46", 2005);
+            Console.WriteLine(myCar.year + " " + myCar.brand + " " + myCar.model);
+        }
+    }
+}
