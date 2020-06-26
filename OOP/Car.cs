1f7e25daaba91365e9573350555fb73a7670baee
omidnb <nobahariomid@gmail.com>
2020-06-25 23:30:30 -0400

    getting started

--- OOP/Car.cs
+++ OOP/Car.cs
@@ -5,10 +5,24 @@ namespace OOP
     class Country
     {
         public string buildIn = "Germany";
+        public int year = 2005;
+        public string brand = " BMW";
+        public void Describe()
+        {
+            Console.WriteLine(year + brand);
+        }
+        
     }
     class Car
     {
         string color = "gray";
+
+        static void Describe2()
+        {
+            Country myObj = new Country();
+            Console.WriteLine(myObj.year + myObj.brand);
+        }
+
         static void Main(string[] args)
         {
             Car myObj1 = new Car();
@@ -16,6 +30,15 @@ namespace OOP
 
             Country myObj2 = new Country();
             Console.WriteLine(myObj2.buildIn);
+            myObj2.year = 2019;
+            Console.WriteLine(myObj2.year);  //2019
+
+            Country myObj3 = new Country();
+            myObj3.year = 2001; //we don't have access to brand because it's not public
+            myObj3.Describe();  //2001 BMW
+
+
+            Describe2();
         }
     }
 }
