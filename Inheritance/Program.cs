b16cb411776998eaf1fe67513477731a1b9df381
omidnb <nobahariomid@gmail.com>
2020-06-26 15:32:59 -0400

    use property

---
+++ Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Car
+    {
+        public string brand;
+        public int hp;
+    }
+
+    class Bmw : Car
+    {
+        public string model = "E46";
+        public int year;
+        public Bmw(string carBrand, int carHp, int carYear)
+        {
+            brand = carBrand;
+            hp = carHp;
+            year = carYear;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bmw myBimmer = new Bmw("BMW", 340, 2005);
+            Console.WriteLine(myBimmer.year + " "+ myBimmer.brand + " " + myBimmer.model + "(" + myBimmer.hp + "hp)");
+        }
+    }
+}
