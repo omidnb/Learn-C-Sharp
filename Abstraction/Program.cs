48b1ab9aaad9d8c9ba61e5969c43d376026db33f
omidnb <nobahariomid@gmail.com>
2020-06-26 20:00:19 -0400

    an example

---
+++ Abstraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Abstraction
+{
+    abstract class Car
+    {
+        //Abstract method (does not have a body)
+        public abstract void carName(); 
+
+        //Regular method
+        public void carModel()
+        {
+            Console.WriteLine("E30");
+        }
+    }
+    class Bmw : Car
+    {
+        public override void carName()
+        {
+            Console.WriteLine("BMW");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bmw myCar = new Bmw();
+            myCar.carName();
+            myCar.carModel();
+        }
+    }
+}
