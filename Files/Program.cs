d3302a2db4300ea8b98eeb4db2f481cb104eadff
omidnb <nobahariomid@gmail.com>
2020-06-28 14:13:07 -0400

    Microsoft .Net File Class Reference Added

--- Files/Program.cs
+++ Files/Program.cs
@@ -6,7 +6,7 @@ namespace Files
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //https://docs.microsoft.com/en-us/dotnet/api/system.io.file?view=netframework-4.8
             string firstTry = "Hello World! :)";
             File.WriteAllText("My_First_File.txt", firstTry);
 
