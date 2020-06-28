5c96370c040bf8fcb562953e118058f179f954eb
omidnb <nobahariomid@gmail.com>
2020-06-28 14:11:09 -0400

    simple example

---
+++ Files/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstTry = "Hello World! :)";
+            File.WriteAllText("My_First_File.txt", firstTry);
+
+            string myText = "\nWelcome.";
+            File.AppendAllText("My_First_File.txt", myText);
+
+            string readText = File.ReadAllText("My_First_File.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
